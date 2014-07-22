874f8d241ac95c5c009f27ef482e7c77dda6079f
unknown <jinyung.jung@JINYUNGJUNGPC02.LGE.NET>
2014-07-22 11:50:18 +0900

    :gem: Remove unuse directive

--- www/js/directives.js
+++ www/js/directives.js
@@ -77,19 +77,6 @@ angular.module('starter.directives', [])
     }
   }
 })
-.directive('bindHtmlCompile', function ($compile) {
-  return function(scope, element, attrs) {
-    scope.$watch(
-      function(scope) {
-        return scope.$eval(attrs.bindHtmlCompile);
-      },
-      function(value) {
-        element.html(value);
-        //$compile(element.contents())(scope);
-      }
-    );
-  };
-})
 .directive('channelUsers', function(UTIL) {
   return {
     restrict: 'A',
@@ -165,75 +152,4 @@ angular.module('starter.directives', [])
       }, DELAY_TIME_BEFORE_POSTING)
     }
   }
-})
-.directive('paperInput', function($parse, $timeout, $browser) {
-  return {
-    restrict: 'E',
-    require: '?ngModel',
-    link: function postLink(scope, element, attrs) {
-
-      var input = element[0];
-      if (attrs.ngModel) {
-        bindNgModel('ngModel', 'inputValue');
-      }
-
-      function bindNgModel(attrName, inputName) {
-        var ngModelGet = $parse(attrs[attrName]);
-        toInput(ngModelGet, attrs[attrName], inputName);
-        toModel(ngModelGet, attrs[attrName], inputName);
-      }
-
-      function toInput(ngModelGet, attrName, inputName) {
-
-        $timeout(function() {
-          input[inputName] = ngModelGet(scope);
-        }, 350);
-
-        var first = true;
-        scope.$watch(attrName, function ngModelWatch() {
-          if (first) {
-            first = false;
-            return;
-          }
-          var value = ngModelGet(scope);
-          input[inputName] = value;
-        });
-      }
-
-      function toModel(modelGet) {
-        var ngModelSet = modelGet.assign;
-
-        var timeout;
-
-        var deferListener = function(ev) {
-          if (!timeout) {
-            timeout = $browser.defer(function() {
-              listener(ev);
-              timeout = null;
-            });
-          }
-        };
-
-        var listener = function(event) {
-          ngModelSet(scope, input.inputValue);
-          scope.$apply();
-        }
-
-        input.addEventListener('change', function(event) {
-          deferListener(event);
-        });
-
-        input.addEventListener('keydown', function(event) {
-          var key = event.keyCode;
-
-          // ignore
-          //    command            modifiers                   arrows
-          if (key === 91 || (15 < key && key < 19) || (37 <= key && key <= 40)) return;
-
-          deferListener(event);
-        });
-
-      }
-    }
-  };
 });
