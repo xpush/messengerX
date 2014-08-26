78fa2196642d6b1bb9ab4928a04c72659a36c4d9
unknown <0nlyoung7@gmail.com>
2014-08-26 23:07:13 +0900

    :gem: Add Speech control

--- www/js/services.js
+++ www/js/services.js
@@ -410,7 +410,7 @@ angular.module('starter.services', [])
             param.message = data.MG;
 
             // Use TTS
-            if( $rootScope.currentScope && $rootScope.currentScope.toggles.useTTS && SpeechSynthesisUtterance ){
+            if( $rootScope.currentScope && $rootScope.currentScope.toggles.useTTS && window.speechSynthesis ){
               var u = new SpeechSynthesisUtterance();
               u.text = data.MG;
               u.lang = 'ko-KR';    
