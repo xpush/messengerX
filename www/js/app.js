2fc43bee7523eba09d5023c6eda0fff010af9565
unknown <0nlyoung7@gmail.com>
2015-10-27 16:58:57 +0900

    bug fix : search user function

--- www/js/app.js
+++ www/js/app.js
@@ -324,6 +324,8 @@ angular.module('messengerx', ['ionic', 'messengerx.controllers', 'messengerx.ser
       }
     }, autoInitFlag );
 
+    $rootScope.xpush.enableDebug();
+
     // rootScope function
     $rootScope.logout = function( callback ){
       Sign.logout(function(){
